b4f8b01e8f681dfc9d4502bb414abcb74644980e
212615385 <benjamin.otte@ge.com>
2018-12-21 10:14:53 -0500

    Added login form for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'welcome/index'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get 'sessions/new'
+  get 'users/new'
   get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'users#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
