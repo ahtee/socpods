2789ef7fbb8e5fc0f81e2a09c2f7570312624536
212615385 <benjamin.otte@ge.com>
2019-01-02 12:09:59 -0500

    users and user generation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  
   def new
+    @user = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get 'sessions/new'
-  get 'users/new'
+  get '/timeline', to: 'home#timeline'
   get '/signup', to: 'users#new'
-  get '/login', to: 'users#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
