cb83b35281e107fa56cea76cbf0ffc67dd020733
212615385 <benjamin.otte@ge.com>
2019-01-02 13:09:42 -0500

    users stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
     # debugger
@@ -8,4 +11,30 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_040813) do
+ActiveRecord::Schema.define(version: 2019_01_02_171015) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2018_12_21_040813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
