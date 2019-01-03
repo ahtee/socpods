e44530e6116f2a90fe7f610d1fe67d65f784fe28
212615385 <benjamin.otte@ge.com>
2019-01-03 15:47:43 -0500

    changing login page to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
+    validates :username, presence: true, length: {minimum: 5}, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255}, 
             format: { with: VALID_EMAIL_REGEX }, 
