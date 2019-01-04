7e99031a900055df9c2a8212e2323ec941f02245
212615385 <benjamin.otte@ge.com>
2019-01-04 10:30:22 -0500

    updating tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { email.downcase! }
-    # validates :name, presence: true, length: { maximum: 50 } # removed this in initial signup form
+    validates :name, length: { maximum: 50 } # removed this in initial signup form
     validates :username, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 100}, 
