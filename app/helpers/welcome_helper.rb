23eec88ac4d175e86b0e365b84572df24e6dc9a8
212615385 <benjamin.otte@ge.com>
2019-01-04 11:06:39 -0500

    formatting

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,12 +1,12 @@
 module WelcomeHelper
     def randomized_background_image
-        images = [  'assets/gifloop.gif', 
-                    'assets/wheat.gif', 
-                    'assets/waves.gif', 
-                    'assets/nightsky.gif', 
-                    'assets/pool.gif', 
-                    'assets/street.gif', 
-                    'assets/world.gif', 
+        images = [  'assets/gifloop.gif',
+                    'assets/wheat.gif',
+                    'assets/waves.gif',
+                    'assets/nightsky.gif',
+                    'assets/pool.gif',
+                    'assets/street.gif',
+                    'assets/world.gif',
                     'assets/hotdog.gif'
                 ]
         images[rand(images.size)]
