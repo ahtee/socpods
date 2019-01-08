07cd76c3c37ee74be4942f340b1c3c4f90cb2721
212615385 <benjamin.otte@ge.com>
2019-01-08 15:06:07 -0500

    Updating profile page and adding delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :email, :password,
+      params.require(:user).permit(:name, :username, :email, :password,
                                    :password_confirmation)
     end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-    def gravatar_for(user, options = { size: 180 })
+    def gravatar_for(user, options = { size: 200 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         size = options[:size]
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
