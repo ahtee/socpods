ebeeb203969faa3fdf5b16939907e9ba28fe6e94
212615385 <benjamin.otte@ge.com>
2019-01-10 13:40:32 -0500

    Changing error messages placement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       redirect_back_or user
     else
       # Create an error message.
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination.'
       render 'new'
     end
   end
