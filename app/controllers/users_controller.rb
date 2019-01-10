c9dc54cc311c7a194a15d178d8f32ec0084d6ea3
212615385 <benjamin.otte@ge.com>
2019-01-10 17:44:34 -0500

    update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  # Choose user_profile layout instead of the default
   layout 'user_profile'
+
   before_action :logged_in_user, only: [:edit, :update, :show]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
