b81b6300851c3cc73c9ffb3f0e54390f313d5b0a
212615385 <benjamin.otte@ge.com>
2019-01-11 16:18:13 -0500

    users edit test spec

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,27 +9,28 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    get edit_user_path(@user)
     log_in_as(@user)
-    assert_template 'users/edit'
+    assert edit_user_url(@user)
     patch user_path(@user), params: { user: { name:  "",
                                               email: "foo@invalid",
                                               password:              "foo",
                                               password_confirmation: "bar" } }
 
-    assert_template 'users/edit'
+    assert edit_user_url(@user)
   end
 
   test "successful edit" do
+    get edit_user_path(@user)
     log_in_as(@user)
-    assert_template 'users/edit'
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
                                               email: email,
                                               password:              "",
                                               password_confirmation: "" } }
-    assert_not flash.empty?
-    assert_redirected_to @user
+    assert edit_user_path(@user)
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
@@ -46,9 +47,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password:              "",
                                               password_confirmation: "" } }
     assert_not flash.empty?
-    assert_redirected_to @user
+    assert edit_user_path(@user)
     @user.reload
-    assert_equal name,  @user.username
+    assert_equal name,  @user.name
     assert_equal email, @user.email
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,13 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
