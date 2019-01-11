50a10be95136de5a3f482da09c72e68eee004e54
212615385 <benjamin.otte@ge.com>
2019-01-11 10:18:03 -0500

    user deleting should work now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   # layout 'user_profile'
 
   before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  # before_action :admin_user,     only: :destroy
 
   def index
     @user = User.new
@@ -47,9 +47,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User has been successfully deleted."
+    User.find_by(username: params[:id]).destroy
     redirect_to root_url
+    flash[:success] = "User has been successfully deleted."
   end
 
   private
