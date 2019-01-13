fdbb4ae835ad5a6509064b013379f8fc5527f797
212615385 <benjamin.otte@ge.com>
2019-01-12 19:54:21 -0500

    need to fix redirect and unit tests for blurbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,17 @@ class UsersController < ApplicationController
     redirect_to root_url
   end
 
+  def createPost 
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+        flash[:success] = "Micropost created!"
+        render 'users/show'
+    else
+        flash[:alert] = "Post not created"
+        render 'users/show'
+    end
+  end
+
   private
 
     def user_params
@@ -70,5 +81,9 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+  end
 
 end
