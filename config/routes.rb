f2119dff1a0b21e8f71df5d5d74f6a6c6e72003b
212615385 <benjamin.otte@ge.com>
2019-01-12 19:34:07 -0500

    Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get ':id', to: 'users#show', as: 'show'
   resources :users , path: '/'
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
