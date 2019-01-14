1296d8922c27b6c72b16bed3fd0ab8903973eecd
212615385 <benjamin.otte@ge.com>
2019-01-14 09:22:05 -0500

    adding user intro to main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if logged_in?
+      @user = current_user
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
       respond_to do |format|
