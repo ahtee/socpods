6c3c5747ac13a0d3fe7934e1cfaabe95a0683af7
ahtee <otteb1@nku.edu>
2019-01-13 22:17:43 -0500

    it's working but i hate it

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
-      respond_modal_with @micropost
       respond_to do |format|
         format.html
         format.js
